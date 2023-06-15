f9233f9ff7ea7c3eed7677a0cd7c322e193e7126
4-o-4 <yuzhakov.lev@gmail.com>
2023-06-15 15:11:59 +0300

    feat: license key formatting v2

--- src/main/java/easy/string/LicenseKeyFormatting.java
+++ src/main/java/easy/string/LicenseKeyFormatting.java
@@ -3,25 +3,20 @@ package easy.string;
 public class LicenseKeyFormatting {
     public String licenseKeyFormatting(String s, int k) {
         StringBuilder str = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            if (str.append(toUpperChar(s.charAt(i))).charAt(i) == '-') {
-                i++;
-                for (int j = 0; i < s.length(); i++) {
-                    char c = s.charAt(i);
-                    if (c != '-') {
-                        if (j == k) {
-                            j = 0;
-                            str.append('-');
-                            str.append(toUpperChar(c));
-                        } else {
-                            str.append(toUpperChar(c));
-                        }
-                        j++;
-                    }
+        for (int i = s.length(), j = 0; i != 0; i--) {
+            char c = s.charAt(i - 1);
+            if (c != '-') {
+                if (j == k) {
+                    j = 0;
+                    str.append('-');
+                    str.append(toUpperChar(c));
+                } else {
+                    str.append(toUpperChar(c));
                 }
+                j++;
             }
         }
-        return str.toString();
+        return str.reverse().toString();
     }
 
     private static char toUpperChar(char c) {
