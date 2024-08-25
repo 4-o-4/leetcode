ae6d8862caf59c98bd74546486846ea938e1598f
4-o-4 <yuzhakov.lev@gmail.com>
2024-08-25 15:20:18 +0400

    refactor: apply prettier

--- src/main/kotlin/easy/string/ClearDigits.kt
+++ src/main/kotlin/easy/string/ClearDigits.kt
@@ -1,18 +1,16 @@
 package easy.string
 
-class ClearDigits {
-    fun clearDigits(s: String): String {
-        val builder = StringBuilder()
-        var isDigit = 0
+fun clearDigits(s: String): String {
+    val builder = StringBuilder()
+    var isDigit = 0
 
-        for (i in s.length - 1 downTo 0) {
-            if (s[i].isDigit())
-                isDigit++
-            else if (isDigit != 0)
-                isDigit--
-            else
-                builder.append(s[i])
-        }
-        return builder.reverse().toString()
+    for (i in s.length - 1 downTo 0) {
+        if (s[i].isDigit())
+            isDigit++
+        else if (isDigit != 0)
+            isDigit--
+        else
+            builder.append(s[i])
     }
+    return builder.reverse().toString()
 }
--- src/test/kotlin/easy/string/ClearDigitsTest.kt
+++ src/test/kotlin/easy/string/ClearDigitsTest.kt
@@ -4,17 +4,15 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class ClearDigitsTest {
-    private val test = ClearDigits()
-
     @Test
     fun example1() {
         val s = "abc"
-        assertEquals("abc", test.clearDigits(s))
+        assertEquals("abc", clearDigits(s))
     }
 
     @Test
     fun example2() {
         val s = "cb34"
-        assertEquals("", test.clearDigits(s))
+        assertEquals("", clearDigits(s))
     }
 }
