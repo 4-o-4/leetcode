10ce4daaeb2d7322b7022437917bf1444b29b77d
4-o-4 <yuzhakov.lev@gmail.com>
2025-11-19 18:38:04 +0400

    feat: 67. Add Binary

--- src/main/python/easy/string/FindTheIndexOfTheFirstOccurrenceInString.py
+++ src/main/python/easy/string/FindTheIndexOfTheFirstOccurrenceInString.py
@@ -1,6 +1,7 @@
 def strStr(haystack: str, needle: str) -> int:
     return haystack.find(needle)
 
+
 if __name__ == '__main__':
-    print(strStr("sadbutsad","sad"))
-    print(strStr("leetcode","leeto"))
+    print(strStr("sadbutsad", "sad"))
+    print(strStr("leetcode", "leeto"))
