6c117be03ada927a4d0d5c967059a9baef0aa94b
4-o-4 <yuzhakov.lev@gmail.com>
2025-12-01 14:57:41 +0400

    feat: 88. Merge Sorted Array

---
+++ src/main/python/easy/array/MergeSortedArray.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    j = 0
+    for i in range(m, m + n):
+        nums1[i] = nums2[j]
+        j += 1
+    nums1.sort()
